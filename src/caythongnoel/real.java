06c2b785a8e673ca04b0d685a1276ef717b2cb83
Your Name <you@example.com>
2023-12-22 23:15:16 +0700

    update color

--- src/caythongnoel/real.java
+++ src/caythongnoel/real.java
@@ -3,64 +3,59 @@ package caythongnoel;
 import java.util.Scanner;
 
 public class real {
-public static void main(String[] args) {
-int n,h,k,o,p;
-Scanner sc = new Scanner(System.in);
-System.out.print("Nhap so tang:");
-n = sc.nextInt();
-sc.close();
-h=2*n-1;
-k=0;
-o=2*n-1;
-for (int c=1; c<=3; c++)
-{
-	for (int i=1; i<=o; i++, k=0)
-	{
-		System.out.print("  ");
-	}
-	while (k!=2*c-1)
-	{
-		k++;
-		System.out.print("* ");
-	}
-	System.out.println();
-	o--;
-}
-o=0;
-for (int i=2; i<=n; i++)
-{
-	o=2*i-1;
-	p=h;
-	for (int c=1; c<=3; c++)
-	{
-		p--;
-		for ( int j=1; j<=p; j++)
-		{
-			System.out.print("  ");
+	public static final String GREEN = "\033[0;32m"; // GREEN
+	public static final String RESET = "\033[0m"; // Text Reset
+	public static final String BROWN = "\033[38;2;139;69;19m"; // Brown text
+
+	public static void main(String[] args) {
+		int n, h, k, o, p;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap so tang:");
+		n = sc.nextInt();
+		sc.close();
+		h = 2 * n - 1;
+		k = 0;
+		o = 2 * n - 1;
+		for (int c = 1; c <= 3; c++) {
+			for (int i = 1; i <= o; i++, k = 0) {
+				System.out.print("  ");
+			}
+			while (k != 2 * c - 1) {
+				k++;
+				System.out.print(GREEN+"* ");
+			}
+			System.out.println();
+			o--;
 		}
-		k=0;
-		while (k!=o)
-		{
-			System.out.print("* ");
-			k++;
+		o = 0;
+		for (int i = 2; i <= n; i++) {
+			o = 2 * i - 1;
+			p = h;
+			for (int c = 1; c <= 3; c++) {
+				p--;
+				for (int j = 1; j <= p; j++) {
+					System.out.print("  ");
+				}
+				k = 0;
+				while (k != o) {
+					System.out.print("* ");
+					k++;
+				}
+				System.out.println();
+				o = o + 2;
+			}
+			h--;
 		}
-		System.out.println();
-		o=o+2;
+		h = 2 * n - 1;
+		System.out.print(RESET);
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= h - 1; j++) {
+				System.out.print("  ");
+			}
+			for (int l = 1; l <= 3; l++) {
+				System.out.print(BROWN+"* ");
+			}
+			System.out.println();
+		}
 	}
-	h--;
-}
-h=2*n-1;
-for (int i=1; i<=n; i++)
-{
-	for (int j=1; j<=h-1; j++)
-	{
-		System.out.print("  ");
-	}
-	for (int l=1; l<=3; l++)
-	{
-		System.out.print("* ");
-	}
-	System.out.println();
-}       
-}
 }
